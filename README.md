c5fb5eb3ee4a23759153b50afd483bf6c0e441b5
xmmmmmovo <13256172360@163.com>
2022-10-07 23:01:02 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# RayTracingInOneWeek
+
+Because I am so vegetable, I only can finish it in one week instead of one weekend.
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+  // Image
+  const int img_width = 256;
+  const int img_height = 256;
+
+  // Render
+  std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n";
+
+  for (int j = img_height - 1; j >= 0; --j) {
+    for (int i = 0; i < img_width; ++i) {
+      auto r = double(i) / (img_width - 1);
+      auto g = double(j) / (img_height - 1);
+      auto b = 0.25;
+
+      int ir = static_cast<int>(255.999 * r);
+      int ig = static_cast<int>(255.999 * g);
+      int ib = static_cast<int>(255.999 * b);
+
+      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+    }
+  }
+
+  return 0;
+}
